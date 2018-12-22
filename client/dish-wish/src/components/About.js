94bba92b97d26079d24d92c333e3f03100ea6e5e
Stephanie Olaya <stepholaya@gmail.com>
2018-12-22 15:39:00 -0500

    built server

---
+++ client/dish-wish/src/components/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class About extends Component{
+  render () {
+    return <section id='about'>
+                <header>
+                    <h1>About DishWish</h1>
+                    <p>"This is where the magic happens."</p>
+                </header>
+            </section>
+  }
+}
+
+
+
+export default About;
