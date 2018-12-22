2846e0ebee3fd33bbe7199d32a2fb17100271986
Robert Beavin <rmbeavin@gmail.com>
2018-12-22 13:13:02 -0500

    updated dw index.js

--- dish-wish/src/index.js
+++ dish-wish/src/index.js
@@ -4,3 +4,11 @@ const express = require ('express');
 const app = express();
 
 app.use(express.static(__dirname + '/client/build'));
+
+app.get('/about/api/', async (req, res) => {
+  let {data} = await axios.get(`http://www.recipepuppy.com/api/?i=${ingredients}`);
+  let recipe = data.results;
+  recipe ? res.send(recipe) : res.send({message: "No recipe found"})
+});
+
+app.listen(5000, () => console.log(`Listening on ${PORT}`));
