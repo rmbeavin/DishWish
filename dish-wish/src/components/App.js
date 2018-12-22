011e1c1729a8160a01eab03e2dd9d1a6b3eb12f4
Jacob Allen <boat.mon@gmail.com>
2018-12-22 12:21:57 -0500

    Adding Home and about page

--- dish-wish/src/components/App.js
+++ dish-wish/src/components/App.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import About from './About';
+import Home from './Home';
+import Header from './Header';
+import Logo from './Logo';
+import Sreach from './Sreach';
+import Info from './Info';
+import Footer from './Footer';
+
 
 export default App;
