83157fc5f3e42cc176d93447db3384b1b6772076
Stephanie Olaya <stepholaya@gmail.com>
2018-12-23 09:59:49 -0500

    fixed errors on index.js and app.js

--- client/dish-wish/src/index.js
+++ client/dish-wish/src/index.js
@@ -1,14 +1,5 @@
-const axios = require('axios');
-const express = require ('express');
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
 
-const app = express();
-
-app.use(express.static(__dirname + '/client/build'));
-
-app.get('/about/api/', async (req, res) => {
-  let {data} = await axios.get(`http://www.recipepuppy.com/api/?i=${ingredients}`);
-  let recipe = data.results;
-  recipe ? res.send(recipe) : res.send({message: "No recipe found"})
-});
-
-app.listen(5000, () => console.log(`Listening on ${PORT}`));
+render(<App />, document.getElementById('root'));
