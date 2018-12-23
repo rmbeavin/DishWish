06c6653531cc0a7521ba93afefd4ef6de8e569be
Jacob Allen <boat.mon@gmail.com>
2018-12-23 11:13:20 -0500

    Fix HTML in Home

--- client/dish-wish/src/components/Home.js
+++ client/dish-wish/src/components/Home.js
@@ -2,27 +2,7 @@ import React, { Component } from 'react';
 
 class Home extends Component{
   render () {
-<<<<<<< Updated upstream
     return (//HTML GOES HERE//
-      <div class="head">
-        //main menu goes here.
-      </div>
-      <div class="logo">
-        <a herf="/" target="_blank">
-          <img scr="https://files.slack.com/files-pri/T055GEHEJ-FF1LCANA3/logo-design-3.png" alt="Logo one">
-        </a>
-      </div>
-      <div class="body">
-      <div class="search">
-        //API goes here, sreach bar.
-      </div>
-      <div class="results">
-        //Results from sreach go here.
-      </div>
-      <div class="foot">
-        <p>All right reserved Jacobaallen.com, copyright 2018.</p>
-=======
-    return (
       <div>
         <div class="head">
           //main menu goes here.
@@ -40,7 +20,6 @@ class Home extends Component{
         <div class="foot">
           <p>All Rights Reserved Copyright 2019 DishWish</p>
         </div>
->>>>>>> Stashed changes
       </div>
     </div>
     )
