a6e73aa5826f22f17968d27d05e6caf3d026eb92
Robert Beavin <rmbeavin@gmail.com>
2018-12-23 12:24:39 -0500

    first search component commit

--- client/dish-wish/src/components/Search.js
+++ client/dish-wish/src/components/Search.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
-class Search extends Component{
+export default class Search extends Component{
   render () {
     return(
-      <div>
-      </div>
-      //HTML GOES HERE//
+      <form onSubmit={getUserInput()}>
+      <h4>Enter your ingredients:</h4>
+      <input placeholder="chicken, carrots, garlic" type="text" name="ingredients"></input>
+      <br></br>
+      <button>Submit</button>
+      </form>
     )
   }
 }
 
-export default Search;
+getRecipes = (e) => {
+    e.preventDefault();
+    const recipes = []
+    console.log(recipes);
+    if(recipes){
+      axios.get(`https://www.recipepuppy.com/api/?i=${ingredients}`).then((res) => {
+        console.log(res.data)
+        const recipeArr = res.data;
+        this.setState({
+          name: recipeArr.name,
+
+        });
+      }).catch((err)) => {
+        this.setState({
+          recipeArr: 'No recipes found!'
+        })
+      }
+    }
+  }
+
+  getUserInput() {
+
+  }
