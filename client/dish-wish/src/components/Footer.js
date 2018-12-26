05c67455c5bd8b17aef02af82a3c090660cba8bf
Jacob Allen <boat.mon@gmail.com>
2018-12-26 16:56:36 -0500

    working links

--- client/dish-wish/src/components/Footer.js
+++ client/dish-wish/src/components/Footer.js
@@ -5,14 +5,20 @@ import {Link} from 'react-router-dom';
 class Footer extends Component{
   render () {
     return (
-      <div>
-        <div className="copyright">
-          <p>All Rights Reserved Copyright 2019 DishWish</p>
-        </div>
+      <div className="bottom">
         <div className="contact">
           <ul>
           <Link to="/contact">Contact Us</Link>
           </ul>
+        </div>
+        <div className="copyright">
+          <p>All Rights Reserved Copyright 2019 DishWish</p>
+        </div>
+        <div className="socials">
+          <img className="facebook" src="facebook.png" alt="  "/>
+          <img className="instagram" src="instagram.png" alt="  "/>
+          <img className="pinterest" src="pinterest.png" alt="  "/>
+          <img className="twitter" src="twitter.png" alt="  "/>
         </div>
       </div>
     )
