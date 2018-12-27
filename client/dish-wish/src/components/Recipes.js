c14eb619fd983032665a6cd0751c0f65ef89a51d
Robert Beavin <rmbeavin@gmail.com>
2018-12-27 11:45:27 -0500

    working api finally

--- client/dish-wish/src/components/Recipes.js
+++ client/dish-wish/src/components/Recipes.js
@@ -8,57 +8,27 @@ import DisplayRecipe from './Cards';
 const words = {
   margin: '40px'
 }
-
 class Display extends React.Component {
   state = {
     recipes: [],
     ingredientName: ''
   }
-
-  handleChange = e => {
+  handleSearchChange = e => {
     this.setState({
       ingredientName: e.target.value
     });
   };
-
   onSubmit = (e) => {
     e.preventDefault();
-    this.setState({
-      ingredientName: e.target.elements.ingredientName.value
-    });
-    console.log(this.state.ingredientName)
-    // let userInput = JSON.stringify(this.state.ingredientName).slice(1,-1);
       axios.get(`http://cors-anywhere.herokuapp.com/recipepuppy.com/api/?i=${this.state.ingredientName}`).then((res) => {
       console.log(res.data.results);
-      // console.log(userInput)
       this.setState({recipes: res.data.results})
       });
   };
-
-  // componentDidUpdate() {
-  //   // let userInput = JSON.stringify(this.state.ingredientName).slice(1,-1);
-  //   axios.get(`http://cors-anywhere.herokuapp.com/recipepuppy.com/api/?i=${this.state.ingredientName}`).then((res) => {
-  //     this.setState({ recipes: res.data.results })
-  //     console.log(res.data.results);
-  //   // console.log(userInput)
-  //   });
-  // }
-
-
-  // componentDidMount() {
-  //   axios.get(`http://cors-anywhere.herokuapp.com/recipepuppy.com/api/?i=chicken`)
-  //   .then(({data}) => {
-  //     this.setState({recipes: data.results})
-  //     console.log(this.state.recipes);
-  //   }).catch(err => {
-  //     console.log(err.message);
-  //   });
-  // }
-
   render() {
     return(
       <div>
-        <Search handleChange={this.handleChange} onSubmit={this.onSubmit} />
+        <Search handleSearchChange={this.handleSearchChange} ingredientValue={this.state.ingredientName} onSubmit={this.onSubmit} />
         <br></br>
         <Grid className="background" columns='equal' centered>
           {this.state.recipes.map((recipe, index) => {
--- client/dish-wish/src/components/Search.js
+++ client/dish-wish/src/components/Search.js
@@ -5,31 +5,12 @@ import Info from './Info';
 
 
 const Search = (props) => {
-  // state = {
-  //   ingredientName: ''
-  // };
-  // handleChange = e => {
-  //   this.setState({
-  //     [e.target.name] : e.target.value
-  //   });
-  // };
-  // onSubmit = (e) => {
-  //   e.preventDefault();
-  //   let userInput = JSON.stringify(this.state.ingredientName).slice(1,-1);
-  //     axios.get(`http://cors-anywhere.herokuapp.com/recipepuppy.com/api/?i=${userInput}`).then((res) => {
-  //     console.log(res.data.results);
-  //     console.log(userInput)
-  //     });
-  // };
-
-// potentially move this back to input
-  // onChange={props.handleChange}
     return (
       <div>
-      <form onSubmit={props.onSubmit}>
-        <input name="ingredientName" placeholder="chicken, rice, tomatoes, etc" />
-        <button>Submit</button>
-      </form>
+        <form onSubmit={props.onSubmit}>
+          <input type="text" name="test" value={props.inputValue} placeholder="chicken, rice, tomatoes, etc" onChange={props.handleSearchChange} />
+          <button>Submit</button>
+        </form>
       </div>
     )
 };
