48f30976b59eaafb9d61b8927f2b5bb282565bc0
Robert Beavin <rmbeavin@gmail.com>
2018-12-27 11:55:30 -0500

    fixed ability to use spaces in search

--- client/dish-wish/src/components/Search.js
+++ client/dish-wish/src/components/Search.js
@@ -8,7 +8,7 @@ const Search = (props) => {
     return (
       <div>
         <form onSubmit={props.onSubmit}>
-          <input type="text" name="test" value={props.inputValue} placeholder="chicken, rice, tomatoes, etc" onChange={props.handleSearchChange} />
+          <input type="text" name="test" value={JSON.stringify(props.inputValue)} placeholder="chicken, rice, tomatoes, etc" onChange={props.handleSearchChange} />
           <button>Submit</button>
         </form>
       </div>
