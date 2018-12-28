34843320cd12218b1e594a74234d6ed68e2f977f
Robert Beavin <rmbeavin@gmail.com>
2018-12-28 14:39:07 -0500

    updates to search

--- client/dish-wish/src/components/Recipes.js
+++ client/dish-wish/src/components/Recipes.js
@@ -16,6 +16,7 @@ class Display extends React.Component {
       ingredientName: e.target.value
     });
   };
+  
   onSubmit = (e) => {
     e.preventDefault();
       axios.get(`http://cors-anywhere.herokuapp.com/recipepuppy.com/api/?i=${this.state.ingredientName}`).then((res) => {
