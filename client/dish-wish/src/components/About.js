c7fe52e0ea6071b11eb0f4a569a0a2d60b8b1690
Stephanie Olaya <stepholaya@gmail.com>
2018-12-29 13:55:53 -0500

    new styles to about and home

--- client/dish-wish/src/components/About.js
+++ client/dish-wish/src/components/About.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../styles/about.css';
-import DemoCarousel from './Carousel';
 
 
 
@@ -13,16 +12,28 @@ class About extends Component{
             <h1 className="aboutTitle">About DishWish</h1>
           </header>
 
-          <div className = "DemoCarousel">
-            <DemoCarousel/>
+          <div class="hero-image">
+            <div class="hero-text">
+              <h1>Food</h1>
+            </div>
           </div>
 
           <div className="fridgetoPlate">
-            <img src="fridge.png" alt="" />
-            <img src="orangearrow.png" alt="" />
-            <img src="computer.png" alt="" />
-            <img src="orangearrow.png" alt="" />
-            <img src="cutlery.png" alt="" />
+            <img className="redFridge"src="redfridge.png" alt="" />
+            <img className="redArrow1"src="redarrow.png" alt="" />
+            <img className="computer"src="computer.png" alt="" />
+            <img className="redArrow2"src="redarrow.png" alt="" />
+            <img className="redPlate"src="redplate.png" alt="" />
+          </div>
+
+          <div>
+            <p className="howItWorks">How it works</p>
+            <div className="howItWorksContent">
+              <p className="step1"> 1. Look at the ingredients you have available</p>
+              <p className="step2">2. Input your ingredients in the searchbar</p>
+              <p className="step3">3. Search</p>
+              <p className="step4">4. Pick a recipe and start cooking!</p>
+            </div>
           </div>
 
           <div className="companyMotto">
@@ -35,27 +46,6 @@ class About extends Component{
               <p className="aboutParagraph"> This is a random recipe generator based on the ingredients you have readily available! DishWish was founded in 2018 by Robert Beavin, Stephanie Olaya and Jacob Allen as a service for people who struggle to come up with recipes on a day to day basis. The recipe is generated instantly, on the click of a button.</p>
             </div>
             <img className="stockedfridge" src="stockedfridge.jpg" alt="" />
-          </div>
-
-          <div className="whyUs">
-            <p className="instantService">Instant service.</p>
-            <p className="convenience">Can be used anywhere, even at the comfort of your own home.</p>
-            <p className="easyToUse"> It is easy to use, just input the ingredients you have on the search bar, and a recipe will be generated.</p>
-          </div>
-
-          <div>
-            <h1 className="highlight">Make your cooking wish come true!</h1>
-            <p className ="highlight2">We have a million recipes for you to try.</p>
-          </div>
-
-          <div>
-            <p className="howItWorks">How it works</p>
-            <div className="howItWorksContent">
-              <p className="step1"> 1. Look at the ingredients you have available</p>
-              <p className="step2">2. Input your ingredients in the searchbar</p>
-              <p className="step3">3. Search</p>
-              <p className="step4">4. Pick a recipe and start cooking!</p>
-            </div>
           </div>
       </div>
     </section>
--- client/dish-wish/src/components/Header.js
+++ client/dish-wish/src/components/Header.js
@@ -6,15 +6,12 @@ class Header extends Component{
   render () {
     return (
       <div className="navbar">
-        <div class="icon">
-          <Link to="/"><img className="icon logo" src="icon.png" alt="  "/></Link>
-        </div>
-        <div class="links">
-        <ul>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/recipes">Recipes</Link>
-        </ul>
+        <div className="links">
+          <ul>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/recipes">Recipes</Link>
+          </ul>
       </div>
         <div className="dropdown">
           <button className="dropbtn">More
@@ -24,7 +21,6 @@ class Header extends Component{
             <ul>
               <Link to="/Contact">Contact Us</Link>
               <Link to="/FAQ">FAQ</Link>
-              <Link to="/Founders">Founders</Link>
             </ul>
           </div>
         </div>
