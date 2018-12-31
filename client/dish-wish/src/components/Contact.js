a6d2d9af15aaf2d1cd0d9db521a0038722634b27
Stephanie Olaya <stepholaya@gmail.com>
2018-12-31 13:05:38 -0500

    added contact component and styling and made styling changes to the search

--- client/dish-wish/src/components/Contact.js
+++ client/dish-wish/src/components/Contact.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react';
+import '../styles/contact.css';
 
 class Contact extends Component{
-  render () {
+  state = {
+    firstName: '',
+    lastName:'',
+    email: '',
+    Question: '',
+  };
+
+  change = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit();
+    this.setState({
+      firstName: '',
+      lastName:'',
+      email: '',
+      Question: '',
+    });
+  }
+
+  render (){
     return (
-      <div className="contact">
-        <p>Please call 1-800-DishWish for support.</p>
+      <div className="content">
+        <p className="contact">Contact Us</p>
+        <form className='form'>
+          <input className='inputfirstname'
+            name='firstName'
+            placeholder='First Name'
+            value={this.state.firstName}
+            onChange={ e => this.setState({ firstName: e.target.value})}/>
+          <input className='inputlastname'
+            name='lastName'
+            placeholder='Last Name'
+            value={this.state.lastName}
+            onChange={ e => this.setState({ lastName: e.target.value})}/>
+          <input className='inputemail'
+            name='email'
+            placeholder='Email'
+            value={this.state.email}
+            onChange={ e => this.setState({ email: e.target.value})}/>
+          <input className='inputquestion'
+            name='Question'
+            placeholder='Question'
+            value={this.state.Question}
+            onChange={ e => this.setState({ Question: e.target.value})}/>
+
+          <button className="contactButton" onClick={(e)=> this.onSubmit}>Submit</button>
+        </form>
       </div>
-    )
+    );
   }
 }
 
